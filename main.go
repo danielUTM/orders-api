ff0045d0191338391d44e3380e8af135f221b0b3
danielUTM <danielcampbell.327@icloud.com>
2025-01-14 23:24:29 +0000

    Added redis set up and graceful shutdown

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/danielUTM/orders-api/application"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start app: ", err)
 	}
